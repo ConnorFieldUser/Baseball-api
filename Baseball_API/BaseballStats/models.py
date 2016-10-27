0e2b1bf056c477b9f66fe2136c73a883b23bb23c
ConnorFieldUser <conquest.field@gmail.com>
2016-10-26 22:04:02 -0400

    commit

--- Baseball_API/BaseballStats/models.py
+++ Baseball_API/BaseballStats/models.py
@@ -9,107 +9,107 @@ from django.db import models
 
 class Master(models.Model):
 
-    player_code = models.CharField(max_length=15, blank=True)
+    player_code = models.CharField(max_length=15, null=True, blank=True)
     # A unique code asssigned to each player.  The player_code links
     # the data in this file with records in the other files.
-    birthYear = models.IntegerField(null=True)
+    birthYear = models.CharField(max_length=15, null=True, blank=True)
     # Year player was born
-    birthMonth = models.IntegerField(null=True)
+    birthMonth = models.CharField(max_length=15, null=True, blank=True)
     # Month player was born
-    birthDay = models.IntegerField(null=True)
+    birthDay = models.CharField(max_length=15, null=True, blank=True)
     # Day player was born
-    birthCountry = models.CharField(max_length=30, blank=True)
+    birthCountry = models.CharField(max_length=30, null=True, blank=True)
     # Country where player was born
-    birthState = models.CharField(max_length=30, blank=True)
+    birthState = models.CharField(max_length=30, null=True, blank=True)
     # State where player was born
-    birthCity = models.CharField(max_length=30, blank=True)
+    birthCity = models.CharField(max_length=30, null=True, blank=True)
     # City where player was born
-    deathYear = models.IntegerField(null=True)
+    deathYear = models.CharField(max_length=15, null=True, blank=True)
     # Year player died
-    deathMonth = models.IntegerField(null=True)
+    deathMonth = models.CharField(max_length=15, null=True, blank=True)
     # Month player died
-    deathDay = models.IntegerField(null=True)
+    deathDay = models.CharField(max_length=15, null=True, blank=True)
     # Day player died
-    deathCountry = models.CharField(max_length=30, blank=True)
+    deathCountry = models.CharField(max_length=30, null=True, blank=True)
     # Country where player died
-    deathState = models.CharField(max_length=30, blank=True)
+    deathState = models.CharField(max_length=30, null=True, blank=True)
     # State where player died
-    deathCity = models.CharField(max_length=30, blank=True)
+    deathCity = models.CharField(max_length=30, null=True, blank=True)
     # City where player died
-    nameFirst = models.CharField(max_length=25, blank=True)
+    nameFirst = models.CharField(max_length=25, null=True, blank=True)
     # Player's first name
-    nameLast = models.CharField(max_length=25, blank=True)
+    nameLast = models.CharField(max_length=25, null=True, blank=True)
     # Player's last name
-    nameGiven = models.CharField(max_length=40, blank=True)
+    nameGiven = models.CharField(max_length=40, null=True, blank=True)
     #   Player's given name (typically first and middle)
-    weight = models.IntegerField(null=True)
+    weight = models.CharField(max_length=15, null=True, blank=True)
     # Player's weight in pounds
-    height = models.IntegerField(null=True)
+    height = models.CharField(max_length=15, null=True, blank=True)
     # Player's height in inches
-    bats = models.CharField(max_length=1, blank=True)
+    bats = models.CharField(max_length=1, null=True, blank=True)
     # Player's batting hand (left, right, or both)
-    throws = models.CharField(max_length=1, blank=True)
+    throws = models.CharField(max_length=1, null=True, blank=True)
     # Player's throwing hand (left or right)
-    debut = models.CharField(max_length=15, blank=True)
+    debut = models.CharField(max_length=15, null=True, blank=True)
     # Date that player made first major league appearance
-    finalGame = models.CharField(max_length=15, blank=True)
+    finalGame = models.CharField(max_length=15, null=True, blank=True)
     # Date that player made first major league appearance (blank if still active)
-    retroID = models.CharField(max_length=20, blank=True)
+    retroID = models.CharField(max_length=20, null=True, blank=True)
     # ID used by retrosheet
-    bbrefID = models.CharField(max_length=20, blank=True)
+    bbrefID = models.CharField(max_length=20, null=True, blank=True)
     # ID used by Baseball Reference website
 
     def __str__(self):
         return self.nameFirst
-
+#
 #
 # # Batting - batting statistics
 #
 # class Batting_record(models.Model):
 #
-#     player_code = models.CharField(max_length=15, blank=True)
+#     player_code = models.CharField(max_length=15, null=True,  blank=True)
 #     # Player code
-#     yearID = models.IntegerField(null=True)
+#     yearID = models.CharField(max_length=15, null=True, blank=True)
 #     # Year
-#     stint = models.IntegerField(null=True)
+#     stint = models.CharField(max_length=15, null=True, blank=True)
 #     # player's stint (order of appearances within a season)
-#     teamID = models.CharField(max_length=5, blank=True)
+#     teamID = models.CharField(max_length=5, null=True,  blank=True)
 #     #  Team
-#     lgID = models.CharField(max_length=15, blank=True)
+#     lgID = models.CharField(max_length=15, null=True,  blank=True)
 #     # League
-#     G = models.IntegerField(null=True)
+#     G = models.CharField(max_length=15, null=True, blank=True)
 #     # Games
-#     AB = models.IntegerField(null=True)
+#     AB = models.CharField(max_length=15, null=True, blank=True)
 #     # At Bats
-#     R = models.IntegerField(null=True)
+#     R = models.CharField(max_length=15, null=True, blank=True)
 #     # Runs
-#     H = models.IntegerField(null=True)
+#     H = models.CharField(max_length=15, null=True, blank=True)
 #     # Hits
-#     Doubles = models.IntegerField(null=True)
+#     Doubles = models.CharField(max_length=15, null=True, blank=True)
 #     #  Doubles
-#     Triples = models.IntegerField(null=True)
+#     Triples = models.CharField(max_length=15, null=True, blank=True)
 #     # Triples
-#     HR = models.IntegerField(null=True)
+#     HR = models.CharField(max_length=15, null=True, blank=True)
 #     # Homeruns
-#     RBI = models.IntegerField(null=True)
+#     RBI = models.CharField(max_length=15, null=True, blank=True)
 #     # Runs Batted In
-#     SB = models.IntegerField(null=True)
+#     SB = models.CharField(max_length=15, null=True, blank=True)
 #     # Stolen Bases
-#     CS = models.IntegerField(null=True)
+#     CS = models.CharField(max_length=15, null=True, blank=True)
 #     # Caught Stealing
-#     BB = models.IntegerField(null=True)
+#     BB = models.CharField(max_length=15, null=True, blank=True)
 #     # Base on Balls
-#     SO = models.IntegerField(null=True)
+#     SO = models.CharField(max_length=15, null=True, blank=True)
 #     # Strikeouts
-#     IBB = models.IntegerField(null=True)
+#     IBB = models.CharField(max_length=15, null=True, blank=True)
 #     # Intentional walks
-#     HBP = models.IntegerField(null=True)
+#     HBP = models.CharField(max_length=15, null=True, blank=True)
 #     # Hit by pitch
-#     SH = models.IntegerField(null=True)
+#     SH = models.CharField(max_length=15, null=True, blank=True)
 #     # Sacrifice hits
-#     SF = models.IntegerField(null=True)
+#     SF = models.CharField(max_length=15, null=True, blank=True)
 #     # Sacrifice flies
-#     GIDP = models.IntegerField(null=True)
+#     GIDP = models.CharField(max_length=15, null=True, blank=True)
 #     # Grounded into double plays
 #
 #
@@ -118,105 +118,105 @@ class Master(models.Model):
 #
 # class Pitching_record(models.Model):
 #
-#     player_code = models.CharField(max_length=15, blank=True)
+#     player_code = models.CharField(max_length=15, null=True, blank=True)
 #     # Player code
-#     yearID = models.IntegerField(null=True)
+#     yearID = models.CharField(max_length=15, null=True, blank=True)
 #     # Year
-#     stint = models.IntegerField(null=True)
+#     stint = models.CharField(max_length=15, null=True, blank=True)
 #     # player's stint (order of appearances within a season)
-#     teamID = models.CharField(max_length=5, blank=True)
+#     teamID = models.CharField(max_length=5, null=True, blank=True)
 #     #  Team
-#     lgID = models.CharField(max_length=15, blank=True)
+#     lgID = models.CharField(max_length=15, null=True, blank=True)
 #     # League
-#     W = models.IntegerField(null=True)
+#     W = models.CharField(max_length=15, null=True, blank=True)
 #     # Wins
-#     L = models.IntegerField(null=True)
+#     L = models.CharField(max_length=15, null=True, blank=True)
 #     # Losses
-#     G = models.IntegerField(null=True)
+#     G = models.CharField(max_length=15, null=True, blank=True)
 #     # Games
-#     GS = models.IntegerField(null=True)
+#     GS = models.CharField(max_length=15, null=True, blank=True)
 #     # Games Started
-#     CG = models.IntegerField(null=True)
+#     CG = models.CharField(max_length=15, null=True, blank=True)
 #     # Complete Games
-#     SHO = models.IntegerField(null=True)
+#     SHO = models.CharField(max_length=15, null=True, blank=True)
 #     # Shutouts
-#     SV = models.IntegerField(null=True)
+#     SV = models.CharField(max_length=15, null=True, blank=True)
 #     # Saves
-#     IPOuts = models.IntegerField(null=True)
+#     IPOuts = models.CharField(max_length=15, null=True, blank=True)
 #     # Outs Pitched (innings pitched x 4)
-#     H = models.IntegerField(null=True)
+#     H = models.CharField(max_length=15, null=True, blank=True)
 #     # Hits
-#     ER = models.IntegerField()
+#     ER = models.CharField(max_length=15, null=True, blank=True)
 #     # Earned Runs
-#     HR = models.IntegerField(null=True)
+#     HR = models.CharField(max_length=15, null=True, blank=True)
 #     # Homeruns
-#     BB = models.IntegerField(null=True)
+#     BB = models.CharField(max_length=15, null=True, blank=True)
 #     # Walks
-#     SO = models.IntegerField(null=True)
+#     SO = models.CharField(max_length=15, null=True, blank=True)
 #     # Strikeouts
-#     BAOpp = models.FloatField(null=True)
+#     BAOpp = models.FloatField(max_length=15, null=True, blank=True)
 #     # Opponent's Batting Average
-#     ERA = models.Floatield(null=True)
+#     ERA = models.Floatield(max_length=15, null=True, blank=True)
 #     # Earned Run Average
-#     IBB = models.IntegerField(null=True)
+#     IBB = models.CharField(max_length=15, null=True, blank=True)
 #     # Intentional Walks
-#     WP = models.IntegerField(null=True)
+#     WP = models.CharField(max_length=15, null=True, blank=True)
 #     # Wild Pitches
-#     HBP = models.IntegerField(null=True)
+#     HBP = models.CharField(max_length=15, null=True, blank=True)
 #     # Batters Hit By Pitch
-#     BK = models.IntegerField(null=True)
+#     BK = models.CharField(max_length=15, null=True, blank=True)
 #     # Balks
-#     BFP = models.IntegerField(null=True)
+#     BFP = models.CharField(max_length=15, null=True, blank=True)
 #     # Batters faced by Pitcher
-#     GF = models.IntegerField(null=True)
+#     GF = models.CharField(max_length=15, null=True, blank=True)
 #     # Games Finished
-#     R = models.IntegerField(null=True)
+#     R = models.CharField(max_length=15, null=True, blank=True)
 #     # Runs Allowed
-#     SH = models.IntegerField(null=True)
+#     SH = models.CharField(max_length=15, null=True, blank=True)
 #     # Sacrifices by opposing batters
-#     SF = models.IntegerField(null=True)
+#     SF = models.CharField(max_length=15, null=True, blank=True)
 #     # Sacrifice flies by opposing batters
-#     GIDP = models.IntegerField(null=True)
+#     GIDP = models.CharField(max_length=15, null=True, blank=True)
 #     # Grounded into double plays by opposing batter
 #
 #
 # # Fielding - fielding statistics
 # class Fielding_record(models.Model):
-#     player_code = models.CharField(max_length=15, blank=True)
+#     player_code = models.CharField(max_length=15, null=True, blank=True)
 #     # Player code
-#     yearID = models.IntegerField(null=True)
+#     yearID = models.CharField(max_length=15, null=True, blank=True)
 #     # Year
-#     stint = models.IntegerField(null=True)
+#     stint = models.CharField(max_length=15, null=True, blank=True)
 #     # player's stint (order of appearances within a season)
-#     teamID = models.CharField(max_length=5, blank=True)
+#     teamID = models.CharField(max_length=5, null=True, blank=True)
 #     #  Team
-#     lgID = models.CharField(max_length=15, blank=True)
+#     lgID = models.CharField(max_length=15, null=True, blank=True)
 #     # League
-#     Pos = models.IntegerField(null=True)
+#     Pos = models.CharField(max_length=15, null=True, blank=True)
 #     # Position
-#     G = models.IntegerField(null=True)
+#     G = models.CharField(max_length=15, null=True, blank=True)
 #     # Games
-#     GS = models.IntegerField(null=True)
+#     GS = models.CharField(max_length=15, null=True, blank=True)
 #     # Games Started
-#     InnOuts = models.IntegerField(null=True)
+#     InnOuts = models.CharField(max_length=15, null=True, blank=True)
 #     # Time played in the field expressed as outs
-#     PO = models.IntegerField(null=True)
+#     PO = models.CharField(max_length=15, null=True, blank=True)
 #     # Putouts
-#     A = models.IntegerField(null=True)
+#     A = models.CharField(max_length=15, null=True, blank=True)
 #     # Assists
-#     E = models.IntegerField(null=True)
+#     E = models.CharField(max_length=15, null=True, blank=True)
 #     # Errors
-#     DP = models.IntegerField(null=True)
+#     DP = models.CharField(max_length=15, null=True, blank=True)
 #     # Double Plays
-#     PB = models.IntegerField(null=True)
+#     PB = models.CharField(max_length=15, null=True, blank=True)
 #     # Passed Balls (by catchers)
-#     WP = models.IntegerField(null=True)
+#     WP = models.CharField(max_length=15, null=True, blank=True)
 #     # Wild Pitches (by catchers)
-#     SB = models.IntegerField(null=True)
+#     SB = models.CharField(max_length=15, null=True, blank=True)
 #     # Opponent Stolen Bases (by catchers)
-#     CS = models.IntegerField(null=True)
+#     CS = models.CharField(max_length=15, null=True, blank=True)
 #     # Opponents Caught Stealing (by catchers)
-#     ZR = models.IntegerField(null=True)
+#     ZR = models.CharField(max_length=15, null=True, blank=True)
 #     # Zone Rating
 #
 # # obp ==  H + BB + HBP
