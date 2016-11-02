0b2e3e985063d497d96d4ae5636cb2db6cda47e6
ConnorFieldUser <conquest.field@gmail.com>
2016-11-02 18:55:39 -0400

    commit

--- Baseball_API/BaseballStats/serializers.py
+++ Baseball_API/BaseballStats/serializers.py
@@ -11,18 +11,38 @@ class MasterSerializer(serializers.ModelSerializer):
 
 
 class Batting_recordSerializer(serializers.ModelSerializer):
+    playerID = serializers.HyperlinkedRelatedField(
+        many=False,
+        view_name="master_detail_update_destroy_api_view",
+        read_only=True,
+    )
+
     class Meta:
         model = Batting_record
         fields = '__all__'
 
 
 class Pitching_recordSerializer(serializers.ModelSerializer):
+
+    playerID = serializers.HyperlinkedRelatedField(
+        many=False,
+        view_name="master_detail_update_destroy_api_view",
+        read_only=True,
+    )
+
     class Meta:
         model = Pitching_record
         fields = '__all__'
 
 
 class Fielding_recordSerializer(serializers.ModelSerializer):
+
+    playerID = serializers.HyperlinkedRelatedField(
+        many=False,
+        view_name="master_detail_update_destroy_api_view",
+        read_only=True,
+    )
+
     class Meta:
         model = Fielding_record
         fields = '__all__'
